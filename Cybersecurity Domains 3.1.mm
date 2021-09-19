6133845f55e7eb8d5c6a19b91028733afb6b42cf
L1ix <86494603+L1ix@users.noreply.github.com>
2021-09-19 10:30:23 +0200

    Add files via upload

---
+++ Cybersecurity Domains 3.1.mm
@@ -0,0 +1,183 @@
+﻿<map version="0.9.0">
+    <node TEXT=" Cybersecurity Domains">
+        <node TEXT="Security Architecture">
+            <node TEXT="Network Design">
+                <node TEXT="DDoS Prevention"></node>
+            </node>
+            <node TEXT="Security Engineering"></node>
+            <node TEXT="Data Protection ">
+                <node TEXT="Data Leakage Prevention"></node>
+            </node>
+            <node TEXT="Access Control">
+                <node TEXT="Identity Management ">
+                    <node TEXT="Privileged Access 
+Management"></node>
+                    <node TEXT="Identity &amp; Access
+Management "></node>
+                </node>
+                <node TEXT="Federated Identity"></node>
+                <node TEXT="MFA &amp; SSO"></node>
+            </node>
+            <node TEXT="Cloud Security"></node>
+            <node TEXT="Secure System Build">
+                <node TEXT="Baseline Configuration "></node>
+                <node TEXT="Patch Management"></node>
+            </node>
+            <node TEXT="Cryptography">
+                <node TEXT="Key and Secret Management">
+                    <node TEXT="Vaulting"></node>
+                    <node TEXT="HSM"></node>
+                </node>
+                <node TEXT="Encryption Standards"></node>
+                <node TEXT="Certificate Management"></node>
+            </node>
+            <node TEXT="Endpoint Hygiene "></node>
+            <node TEXT="Container Security"></node>
+        </node>
+        <node TEXT="Security Operation ">
+            <node TEXT="Security Operation Centers"></node>
+            <node TEXT="Incident Response ">
+                <node TEXT="Breach Notification "></node>
+                <node TEXT="Containment "></node>
+                <node TEXT="Eradication "></node>
+                <node TEXT="Investigation ">
+                    <node TEXT="Forensics "></node>
+                </node>
+                <node TEXT="Blue Team"></node>
+                <node TEXT="Red Team"></node>
+                <node TEXT="Detection"></node>
+            </node>
+            <node TEXT="SIEM">
+                <node TEXT="SOAR"></node>
+            </node>
+            <node TEXT="Vulnerability
+Management"></node>
+            <node TEXT="Active Defense"></node>
+            <node TEXT="Threat Hunting"></node>
+        </node>
+        <node TEXT="Governance ">
+            <node TEXT="Laws and Regulations">
+                <node TEXT="Regional">
+                    <node TEXT="CCPA"></node>
+                    <node TEXT="NYS-DFS 23 NYCRR 500"></node>
+                </node>
+                <node TEXT="Central Government">
+                    <node TEXT="GDPR"></node>
+                    <node TEXT="GLBA"></node>
+                </node>
+                <node TEXT="Industry Specific ">
+                    <node TEXT="PCI"></node>
+                    <node TEXT="HIPAA"></node>
+                </node>
+            </node>
+            <node TEXT="Company's Written Policies">
+                <node TEXT="Policy"></node>
+                <node TEXT="Procedure "></node>
+                <node TEXT="Standard"></node>
+                <node TEXT="Guideline "></node>
+                <node TEXT="Compliance &amp; Enforcement "></node>
+            </node>
+            <node TEXT="Executive Management Involvement ">
+                <node TEXT="Reports and Scorecards">
+                    <node TEXT="KPIs/KRIs"></node>
+                </node>
+                <node TEXT="Risk Informed"></node>
+            </node>
+        </node>
+        <node TEXT="Risk Assessment ">
+            <node TEXT="3rd Party Risk ">
+                <node TEXT="4th Party Risk"></node>
+            </node>
+            <node TEXT="Penetration test">
+                <node TEXT="Infrastructure
+(Network and Systems)"></node>
+                <node TEXT="Application Pen Tests"></node>
+                <node TEXT="Social Engineering "></node>
+                <node TEXT="DAST"></node>
+            </node>
+            <node TEXT="Vulnerability
+scan"></node>
+            <node TEXT="Assets Inventory "></node>
+            <node TEXT="Risk Monitoring Services
+(Risk score)"></node>
+        </node>
+        <node TEXT="User Education">
+            <node TEXT="Training (new skills)"></node>
+            <node TEXT="Awareness (reinforcement) "></node>
+            <node TEXT="Cyber security table-top
+exercise "></node>
+        </node>
+        <node TEXT="Threat Intelligence ">
+            <node TEXT="Internal ">
+                <node TEXT="IOCs"></node>
+                <node TEXT="Intel. Sharing "></node>
+            </node>
+            <node TEXT="External">
+                <node TEXT="Contextual"></node>
+            </node>
+        </node>
+        <node TEXT="Career Development">
+            <node TEXT="Training"></node>
+            <node TEXT="Certifications"></node>
+            <node TEXT="Conferences "></node>
+            <node TEXT="Peer Groups"></node>
+            <node TEXT="Self Study"></node>
+            <node TEXT="Coaches and
+Role Models"></node>
+        </node>
+        <node TEXT="Frameworks
+and Standards ">
+            <node TEXT="NIST Cybersecurity
+Framework"></node>
+            <node TEXT="ISO 27001
+27017
+27018"></node>
+            <node TEXT="OWASP Top 10
+(WebApp &amp; API)"></node>
+            <node TEXT="CIS Top 20 Controls
+CIS Benchmarks"></node>
+            <node TEXT="MITRE 
+ATT&amp;CK 
+Framework"></node>
+        </node>
+        <node TEXT="Physical Security">
+            <node TEXT="IoT Security"></node>
+        </node>
+        <node TEXT="Enterprise Risk Management">
+            <node TEXT="Lines of Defense ">
+                <node TEXT="3. Audit">
+                    <node TEXT="SOC1/SOC2"></node>
+                </node>
+                <node TEXT="1. Process Owners"></node>
+                <node TEXT="2. Risk Mgmt Group"></node>
+            </node>
+            <node TEXT="Risk Treatment
+Actions"></node>
+            <node TEXT="Risk Appetite "></node>
+            <node TEXT="Cyber Insurance"></node>
+            <node TEXT="BCP/DR"></node>
+            <node TEXT="Crisis Management"></node>
+            <node TEXT="Risk
+Acceptance
+Statement"></node>
+            <node TEXT="Risk Register"></node>
+        </node>
+        <node TEXT="Application Security">
+            <node TEXT="S-SDLC">
+                <node TEXT="&quot;Shift Left&quot;">
+                    <node TEXT="CI/CD integration"></node>
+                </node>
+            </node>
+            <node TEXT="Source Code Scan">
+                <node TEXT="Open Source Scan"></node>
+                <node TEXT="SAST"></node>
+            </node>
+            <node TEXT="Data-Flow Diagram"></node>
+            <node TEXT="API Security"></node>
+            <node TEXT="Security UX"></node>
+            <node TEXT="Security QA"></node>
+        </node>
+    </node>
+    <node TEXT="The Map of Cybersecurity Domains 
+Henry Jiang | March 2021 | REV 3.1"></node>
+</map>
